97757eb9a002607bf22386c25b4f05db2eca7410
ManuelAgustinLucero <apattyeffect@gmail.com>
2017-03-28 22:31:56 -0300

    commit inicial

--- eje10.rb
+++ eje10.rb
@@ -7,31 +7,35 @@ puts "Para abrir el eje6 precione el numero 6"
 puts "Para abrir el eje7 precione el numero 7"
 puts "Para abrir el eje8 precione el numero 8"
 puts "Para abrir el eje9 precione el numero 9"
-puts "Para abrir el eje10 precione el numero 10"
-puts "Para salir precione cualquier otra tecla"
+puts "Para salir precione n "
 
 print "Seleccione su opcion "
 menu = gets.chomp
 
-case menu
-when "1"
-  puts "hola mundo jajaj"
-when "2"
-  load 'eje2.rb'
-when "3"
-  load 'eje3.rb'
-when "4"
-  load 'eje4.rb'
-when "5"
-  load 'eje5.rb'
-when "6"
-  load 'eje6.rb'
-when "7"
-  puts "No disponible"
-when "8"
-  load 'eje8.rb'
-when "9"
-  load 'eje9.rb'
-else
-  puts "Gracias por usar mi menu"
+while menu!="n"
+  case menu
+  when "1"
+    load 'eje1.rb'
+  when "2"
+    load 'eje2.rb'
+  when "3"
+    load 'eje3.rb'
+  when "4"
+    load 'eje4.rb'
+  when "5"
+    load 'eje5.rb'
+  when "6"
+    load 'eje6.rb'
+  when "7"
+    puts "No disponible"
+  when "8"
+    load 'eje8.rb'
+  when "9"
+    load 'eje9.rb'
+  else
+    puts "tecla erronea"
+  end
+  print "Seleccione su opcion o precione n para salir "
+  menu = gets.chomp
 end
+system "clear"
